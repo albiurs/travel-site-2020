1fea821332dc3d19f9c39c620b6749e85b644417
Urs Albisser <u.albisser@gmail.com>
2020-04-17 10:09:45 +0200

    add basic css support to webpck

--- webpack.config.js
+++ webpack.config.js
@@ -7,5 +7,17 @@ module.exports = {
         path: path.resolve(__dirname, 'app')
     },
     mode: 'development', // current project mode (development or production)
-    watch: true    // watch and automatically rebuild on change
+    watch: true,    // watch and automatically rebuild on change
+    module: {
+        rules: [
+            /*
+            style-loader: css-style-support for webpack (which only understands js)
+            css-loader: css-import-support for webpack (which only understands js)
+             */
+            {
+                test: /\.css$/i,        // only if a file ends in ".css"...
+                use: ['style-loader', 'css-loader']     // ...only then use style-loader and also css-loader module.
+            }
+        ]
+    }
 }
