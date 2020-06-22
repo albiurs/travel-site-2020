0736cf1f6d83336461d4a4b4df953abc48d89ad4
Urs Albisser <u.albisser@gmail.com>
2020-06-22 13:23:07 +0200

    build update clean and chunk files

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 // == constants ==
 const currentTask = process.env.npm_lifecycle_event;     // = dev or build, depending on the according npm script, currently being executed
 const path = require('path');    // import/require-in the node.js 'path' module/package in ./node_modules/@types/node/path.d.ts, which is part of the node library
+//const { SplitChunksPlugin } = require('webpack');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');   // Plugin to clean dist folder on re-build
 
 //post-css plugin array
 const postCSSPlugins = [
@@ -47,7 +49,7 @@ if (currentTask == 'dev') {
          */
         filename: 'bundled.js',                 // property: define name of bundled js file
         path: path.resolve(__dirname, 'app')    // property: define abs. path for bundled js file. path.resolve(__dirname=from/current/dir/, 'to/relative/folder')
-    }
+    };
 
     /*
     setup dev server object @localhost:portNumber
@@ -60,33 +62,29 @@ if (currentTask == 'dev') {
         hot: true,                                  // property: hot module replacement: inject css into javascript into the browsers memory on the fly without the need of reloading the page.
         port: 3000,                                 // property: web-server port
         host: '0.0.0.0'                             // property: make dev server available for all hosts in the local network
-    }
+    };
 
     config.mode = 'development'; // current project mode (development or production)
 }
 
 if (currentTask == 'build') {
     config.output = {                           // property: create js object, which controls the webpack output file
-        /*
-        create output of bundled.js file to be imported in index.html:
-        <script src="bundled.js"></script>
-         */
-        filename: 'bundled.js',                 // property: define name of bundled js file
-        path: path.resolve(__dirname, 'dist')    // property: define abs. path for bundled js file. path.resolve(__dirname=from/current/dir/, 'to/relative/folder')
-    }
+        
+        filename: '[name].[chunkhash].js',      // automatically create build file name 'main.*.js' with junk hashes for re-caching by browser on re-build
+        chunkFilename: '[name].[chunkhash].js', // automatically create build file names 'modal.*.js' and vendors-main.*.js with junk hashes for re-caching by browser on re-build
+        path: path.resolve(__dirname, 'dist')   // property: define abs. path for bundled js files. path.resolve(__dirname=from/current/dir/, 'to/relative/folder')
+    };
 
     config.mode = 'production';
+
+    // splitt bundled.js file into chunks (main.js, modal.js and vendor-main.js)
+    config.optimization = {
+        splitChunks: {chunks: 'all'}
+    };
+
+    // leverage plugins within the build process
+    config.plugins = [new CleanWebpackPlugin()];
 }
-
-
-// == modules ==
-/**
- * module.exports = {javascript object}
- * set up webpack:
- * -> properties will be exported and used by webpack
- *
- * @type {{output: {path: string, filename: string}, mode: string, entry: string, watch: boolean, module: {rules: [{test: RegExp, use: [string, string, {loader: string, options: {plugins: [((arg: simpleVars.ISimpleVarsCallableArgument) => any) | ((arg: simpleVars.ISimpleVarsArgument) => any) | simpleVars, Plugin<nested.Options> | nested, *]}}]}]}}}
- */
 
 
 module.exports = config;
