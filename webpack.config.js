374a58e60599929419d20a810922808f240d21f6
Urs Albisser <u.albisser@gmail.com>
2020-06-22 11:02:51 +0200

    start build config / fix index.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 // == constants ==
-const path = require('path')    // import/require-in the node.js 'path' module/package in ./node_modules/@types/node/path.d.ts, which is part of the node library
+const currentTask = process.env.npm_lifecycle_event;     // = dev or build, depending on the according npm script, currently being executed
+const path = require('path');    // import/require-in the node.js 'path' module/package in ./node_modules/@types/node/path.d.ts, which is part of the node library
 
 //post-css plugin array
 const postCSSPlugins = [
@@ -9,42 +10,12 @@ const postCSSPlugins = [
     require('postcss-nested'),
     require('postcss-hexrgba'),
     require('autoprefixer')
-]
+];
 
 
-// == modules ==
-/**
- * module.exports = {javascript object}
- * set up webpack:
- * -> properties will be exported and used by webpack
- *
- * @type {{output: {path: string, filename: string}, mode: string, entry: string, watch: boolean, module: {rules: [{test: RegExp, use: [string, string, {loader: string, options: {plugins: [((arg: simpleVars.ISimpleVarsCallableArgument) => any) | ((arg: simpleVars.ISimpleVarsArgument) => any) | simpleVars, Plugin<nested.Options> | nested, *]}}]}]}}}
- */
-module.exports = {
-    entry: './app/assets/scripts/App.js',       // property: js file to be bundled with the object
-    output: {                                   // property: create js object, which controls the webpack output file
-        /*
-        create output of bundled.js file to be imported in index.html:
-        <script src="bundled.js"></script>
-         */
-        filename: 'bundled.js',                 // property: define name of bundled js file
-        path: path.resolve(__dirname, 'app')    // property: define abs. path for bundled js file. path.resolve(__dirname=from/current/dir/, 'to/relative/folder')
-    },
-    /*
-    setup dev server object @localhost:portNumber
-     */
-    devServer: {
-        before: function(app, server) {
-            server._watch('./app/**/*.html')    // watch for file changes
-        },
-        contentBase: path.join(__dirname, 'app'),   // property: abs. path to point the dev server
-        hot: true,                                  // property: hot module replacement: inject css into javascript into the browsers memory on the fly without the need of reloading the page.
-        port: 3000,                                 // property: web-server port
-        host: '0.0.0.0'                             // property: make dev server available for all hosts in the local network
-    },
-    mode: 'development', // current project mode (development or production)
-    watch: true,    // Only needed for webpack(npm run build), but not for webpack-dev-server(npm run dev) with devServer.
-                    // Watch the entry file declared above and automatically rebuild on change (App.js and also all its import files)
+// == variables ==
+let config = {
+    entry: './app/assets/scripts/App.js',
     module: {
         rules: [
             /*
@@ -65,4 +36,57 @@ module.exports = {
             }
         ]
     }
+};
+
+
+if (currentTask == 'dev') {
+    config.output = {                           // property: create js object, which controls the webpack output file
+        /*
+        create output of bundled.js file to be imported in index.html:
+        <script src="bundled.js"></script>
+         */
+        filename: 'bundled.js',                 // property: define name of bundled js file
+        path: path.resolve(__dirname, 'app')    // property: define abs. path for bundled js file. path.resolve(__dirname=from/current/dir/, 'to/relative/folder')
+    }
+
+    /*
+    setup dev server object @localhost:portNumber
+     */
+    config.devServer = {
+        before: function(app, server) {
+            server._watch('./app/**/*.html')    // watch for file changes
+        },
+        contentBase: path.join(__dirname, 'app'),   // property: abs. path to point the dev server
+        hot: true,                                  // property: hot module replacement: inject css into javascript into the browsers memory on the fly without the need of reloading the page.
+        port: 3000,                                 // property: web-server port
+        host: '0.0.0.0'                             // property: make dev server available for all hosts in the local network
+    }
+
+    config.mode = 'development'; // current project mode (development or production)
 }
+
+if (currentTask == 'build') {
+    config.output = {                           // property: create js object, which controls the webpack output file
+        /*
+        create output of bundled.js file to be imported in index.html:
+        <script src="bundled.js"></script>
+         */
+        filename: 'bundled.js',                 // property: define name of bundled js file
+        path: path.resolve(__dirname, 'dist')    // property: define abs. path for bundled js file. path.resolve(__dirname=from/current/dir/, 'to/relative/folder')
+    }
+
+    config.mode = 'production';
+}
+
+
+// == modules ==
+/**
+ * module.exports = {javascript object}
+ * set up webpack:
+ * -> properties will be exported and used by webpack
+ *
+ * @type {{output: {path: string, filename: string}, mode: string, entry: string, watch: boolean, module: {rules: [{test: RegExp, use: [string, string, {loader: string, options: {plugins: [((arg: simpleVars.ISimpleVarsCallableArgument) => any) | ((arg: simpleVars.ISimpleVarsArgument) => any) | simpleVars, Plugin<nested.Options> | nested, *]}}]}]}}}
+ */
+
+
+module.exports = config;
